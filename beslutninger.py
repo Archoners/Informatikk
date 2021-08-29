716f8c64c420da4a8d0694dd2889be4c08ecbf2d
Archoners <88625929+Archoners@users.noreply.github.com>
2021-08-29 16:58:46 +0200

    Add files via upload

--- beslutninger.py
+++ beslutninger.py
@@ -1,7 +1,14 @@
+#Spør brukeren om de vil ha brus
 brus = input("Vil du ha brus?")
-if brus == "Ja":
+#bruker .lower() sånn at brukeren skal ha muligheten å skrive ja eller nei på hvilken som helst måte eg. (JA,jA,NEi,nEI osv)
+brus = brus.lower()
+#Bruke if for å sjekke om brukeren svarer "ja" på om han vil ha brus
+if brus == "ja":
     print("Her har du en brus!")
-if brus == "Nei":
+#Samme som forrige men elif sjekker om brukeren skriver nei
+elif brus == "nei":
     print("Den er grei.")
+#bruker else: kommando for å sjekke om brukeren skriver noe annet enn ja eller nei
 else:
     print("Det forstod jeg ikke helt.")
+    
--- dato.py
+++ dato.py
@@ -1,10 +1,21 @@
-day = input("Si meg en random dag!")
-month = input("Si meg en random maaned!")
-newday = input("Si meg en dag til!")
-newmonth = input("Si meg en maaned til!")
-if day + month < newday + newmonth:
- print("Riktig rekefolge!")
- if day + month > newday + newmonth:
-  print("Feil Rekefolkge!")
-if day + month == newday + newmonth:
-    print("Samme dato!")
+#Få verdien av alle 4 variabler. Bruker int fordi vi vill bare ha heltall.
+day = int (input("Si meg en random dag!"))
+month = int (input("Si meg en random maaned!"))
+newday = int (input("Si meg en dag til!"))
+newmonth = int (input("Si meg en maaned til!"))
+#Sjekker om variabelen for "Month" er mindre enn "newmonth". Fordi det er bare snakk om dag og måned og ikke år, det er mulig å få vite om det er riktig rekefølge ved at month er mindre enn new month.
+if month < newmonth:
+ print("Riktig Rekefølge!")
+ #Samme som forrige men bare omvendt. Fordi det er ingen snakk om år så vet vi at det er feil rekefølge ved kunnskapen at newmonth er mindre enn month
+elif month > newmonth:
+ print("Feil Rekefølge!")
+else:
+  #Hvis month og new month er lik (=) da sjekker programmet dagene, same prinsippet følges her og 
+  if day < newday:
+   print("Riktig Rekefølge")
+   
+  elif day > newday:
+   print("Feil Rekefølge")
+   #Else kommando brukes her for situasjoner hvor rekefølgen er samme (=). Fordi vi har bare <,> og = så er det mulig å bruke else
+  else:
+    print("Samme Rekefølge!")
